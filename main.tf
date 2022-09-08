43f2a5df64ae0a29b820a8c6805bcdcc50f4dd0a
naikpriti <91533691+naikpriti@users.noreply.github.com>
2022-09-08 16:02:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,14 @@
 resource "azurerm_dns_zone" "primary_child" {
   name                = "${local.dns.primary_prefix}.${local.dns.domain}"
-  resource_group_name = local.dns.resource_group_name
-  tags                = module.primary_metadata.tags
+  resource_group_name = "test-grp"
 }
 
-     
-resource "azurerm_dns_ns_record" "primary_child" {
-  name                = local.dns.primary_prefix
-  zone_name           = local.dns.domain
-  resource_group_name = local.dns.resource_group_name
-  ttl                 = 300
-  tags                = module.primary_metadata.tags
-  records             = azurerm_dns_zone.primary_child.name_servers
-}
   
 resource "azurerm_dns_cname_record" "record" {
   for_each            = local.dns.cname_records
   name                = each.key
   zone_name           = local.dns.domain
-  resource_group_name = local.dns.resource_group_name
+  resource_group_name = "test-grp"
   ttl                 = 300
   record              = each.value.record
 }
